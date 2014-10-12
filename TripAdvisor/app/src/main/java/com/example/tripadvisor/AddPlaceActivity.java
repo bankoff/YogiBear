23c8867f45c0d4059031feec0bd86b3f2c2bc3de
Rosica Boykova <rossi_308@abv.bg>
2014-10-12 14:38:46 +0300

    Some button functionality added

--- TripAdvisor/app/src/main/java/com/example/tripadvisor/AddPlaceActivity.java
+++ TripAdvisor/app/src/main/java/com/example/tripadvisor/AddPlaceActivity.java
@@ -1,19 +1,52 @@
 package com.example.tripadvisor;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+import android.content.Context;
 
 
-public class AddPlaceActivity extends Activity {
+public class AddPlaceActivity extends Activity implements View.OnClickListener {
+
+    private Button btnAddPicture, btnCreatePlace;
+    final Context context = this;
+    private ImageView mView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_place);
+
+        btnAddPicture = (Button) this.findViewById(R.id.picture_button);
+        btnCreatePlace = (Button) this.findViewById(R.id.create_button);
+        btnAddPicture.setOnClickListener(this);
+        btnCreatePlace.setOnClickListener(this);
+        mView = (ImageView) findViewById(R.id.imageView3);
     }
 
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // TODO Auto-generated method stub
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap photo = (Bitmap) extras.get("data");
+            mView.setImageBitmap(photo);
+        }
+    }
+
+    private void startCamera() {
+        Intent camera = new Intent(
+                android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        this.startActivityForResult(camera, 100);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -32,5 +65,18 @@ public class AddPlaceActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.picture_button) {
+            Toast.makeText(context, "Make pic with the camera", Toast.LENGTH_SHORT)
+                    .show();
+            startCamera();
+        }
+        if (view.getId() == R.id.create_button) {
+            Toast.makeText(context, "Send info to the database and create the place", Toast.LENGTH_SHORT)
+                    .show();
+        }
     }
 }
